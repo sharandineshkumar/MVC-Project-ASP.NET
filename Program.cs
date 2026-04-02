ee088dd9f601a63d9a58494a5f59fb4f42229027
SHARAN <147370511+sharandineshkumar@users.noreply.github.com>
2026-04-02 16:52:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+    });
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Complaints}/{action=Index}/{id?}");
+
+app.Run();
